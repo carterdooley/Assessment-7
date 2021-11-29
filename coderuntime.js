8e7e0d7eab9b23127ddd36d6e3830a5f7cea04cd
Carter Dooley <carter.j.dooley43@gmail.com>
2021-11-28 20:02:27 -0600

    Unique Chars Edit

--- coderuntime.js
+++ coderuntime.js
@@ -31,3 +31,28 @@ console.log('Runtime is', finshed.preciseWords)
 
 
 //Unique Characters
+
+const hasUniqueChars = (str) => {
+    let val = true
+    let k = str.charCodeAt(x)
+    let j = str.charCodeAt(i)
+
+    for(i=0; i < str.length; i++) {
+        let x = 1
+            if (j === k && x !== i) {
+                str = str.substr(x-1)
+                 return val = false
+            }else {
+                return val
+            }  x++
+           
+        }
+    } 
+
+    perf.start();
+    console.log(hasUniqueChars('Hello World'))
+    let finshed2 = perf.stop()
+
+    console.log('Runtime is', finshed2.preciseWords)
+
+    //Runtime = O(1)
